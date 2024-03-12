572a34ca2f4192a0ad34b0766cb8147e7c99c079
jessmarivandalahigon <jessmarchuy22@gmail.com>
2024-03-12 08:21:20 +0800

    added icon button and alert

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonAlert, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -23,9 +24,25 @@ const Tab1: React.FC = () => {
         <IonCardTitle>Student</IonCardTitle>
         <IonCardSubtitle>Jessmar Ivan Dalahigon</IonCardSubtitle>
       </IonCardHeader>
-
       <IonCardContent> Nothing more, nothing less.</IonCardContent>
+
+
+      <IonButton id="present-alert"expand="full">Quick facts</IonButton>
+   
+   <IonAlert
+     trigger="present-alert"
+     header="A Short Title Is Best"
+     subHeader="A Sub Header Is Optional"
+     message="A message should be a short, complete sentence."
+     buttons={['Action']}
+   ></IonAlert>
+
+
+
     </IonCard>
+
+   
+
       </IonContent>
     </IonPage>
   );
