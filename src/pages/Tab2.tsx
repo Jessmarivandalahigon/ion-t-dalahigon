748a94056ed4d9aea68e78492aefa843bacf61b4
jessmaRIVANDALAHIGON <jessmarchuy22@gmail.com>
2024-04-05 08:01:11 +0800

    i add calculator code in tab3

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonActionSheet } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -28,10 +27,10 @@ const Tab2: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         
-        <p>Click count: {clickCount}</p> <b></b>
+        <p>Click count: {clickCount}</p>
         
         <IonButton onClick={handleClick} expand="block">Click me</IonButton>
-        <IonButton onClick={handleReset} expand="block">Reset</IonButton> {/* Add reset button */}
+        <IonButton onClick={handleReset} expand="block">Reset</IonButton>
 
       </IonContent>
     </IonPage>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,22 +1,78 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [input, setInput] = useState(''); // State to store user input
+  const [result, setResult] = useState(''); // State to store calculation result
+
+  // Function to handle button clicks
+  const handleButtonClick = (value: string) => {
+    if (value === '=') {
+      // Evaluate the input expression and update the result state
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      // Clear the input and result
+      setInput('');
+      setResult('');
+    } else {
+      // Append the clicked button value to the input
+      setInput(input + value);
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Calculator</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Calculator</IonTitle>
           </IonToolbar>
         </IonHeader>
-       
+        
+        <div className="calc-container">
+          <div className="calc-input">{input}</div>
+          <div className="calc-result">{result}</div>
+          <IonGrid>
+            <IonRow>
+              <IonCol><IonButton onClick={() => handleButtonClick('7')}>7</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('8')}>8</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('9')}>9</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('/')}>/</IonButton></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol><IonButton onClick={() => handleButtonClick('4')}>4</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('5')}>5</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('6')}>6</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('*')}>*</IonButton></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol><IonButton onClick={() => handleButtonClick('1')}>1</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('2')}>2</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('3')}>3</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('-')}>-</IonButton></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol><IonButton onClick={() => handleButtonClick('0')}>0</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('.')}>.</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('=')}>=</IonButton></IonCol>
+              <IonCol><IonButton onClick={() => handleButtonClick('+')}>+</IonButton></IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol size="6"><IonButton onClick={() => handleButtonClick('C')}>C</IonButton></IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+        
       </IonContent>
     </IonPage>
   );
