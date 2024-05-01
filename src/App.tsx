2b917cd0398c1634e22c23ab93cff6a16d820a06
jessmarivandalahigon <jessmarchuy22@gmailcom>
2024-05-01 19:28:52 +0800

    added code and make some small changes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import Home from './pages/home';
 import Profile from './pages/Profile';
 import Clickcounter from './pages/Clickcounter';
 import calculator from './pages/Calculator';
+import QuoteGenerator from './pages/qoutesgenerator';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -74,7 +75,7 @@ const App: React.FC = () => (
 
 
           <Route path="/quotegenerator">
-            <QuoteGenerator />
+            <quoteGenerator />
           </Route>
 
 
