25e03186e361006c6307d47c031aa45a1f8595cf
jessmarivandalahigon <jessmarchuy22@gmailcom>
2024-05-01 19:16:37 +0800

    added quotesgenerator.tsx/css

--- src/App.tsx
+++ src/App.tsx
@@ -73,10 +73,17 @@ const App: React.FC = () => (
           </Route>
 
 
-          
+
+
+
+
           <Route exact path="/">
             <Redirect to="/home" />
           </Route>
+
+
+
+
 
 
         </IonRouterOutlet>
