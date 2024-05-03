0c0543eed8f1b073ffacb97f369a41f1558b051f
jessmarivandalahigon <jessmarchuy22@gmail.com>
2024-05-03 08:07:27 +0800

    added something

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,65 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton,
-    IonIcon
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  IonItemDivider,
+  IonSearchbar,
+  IonBadge
 } from '@ionic/react';
-import { useHistory } from 'react-router-dom';
-import { alarmOutline, calculatorOutline, clipboardOutline } from 'ionicons/icons';
+
+
+//Ionic Icons
+import { speedometerOutline,calculator,pencil, chatbubble, readerOutline, logoIonic,logoFirebase, logoReact, book} from 'ionicons/icons';
+
+const cardData = [
+  {
+    title: 'Click Counter',
+    icon: speedometerOutline,
+    link: '/Clickcounter',
+    tags: {
+      tag1: logoIonic,
+    }
+
+  },
+  {
+    title: 'Calculator',
+    icon: calculator,
+    link: '/Calculator',
+    tags: {
+      tag1: logoIonic,
+    }
+  },
+  {
+    title: 'To Do List',
+    icon: book,
+    link: '/todolist',
+    tags: {
+      tag1: logoIonic,
+    }
+  },
+  {
+    title: 'Quote Generator',
+    icon: chatbubble,
+    link: '/quotegenerator',
+    tags: {
+      tag1: logoIonic,
+    }
+  },
+  {
+    title: 'Notes',
+    icon: readerOutline,
+
+    link: '/notes',
+    tags: {
+      tag1: logoIonic,
+    }
+  }
+  
+];
 
 const Home: React.FC = () => {
-    const history = useHistory();
-
-    const goToCalculator = () => {
-        history.push('/calculator');
-    };
-
+  {/*Dynamic Search*/}
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Home</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Home</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-
-                {/* Add IonCard */}
-                <IonCard>
-                    
-                    <IonCardContent>
-                        <IonButton expand="block" routerLink='/calculator'>
-                            <IonIcon icon={calculatorOutline} slot="start" />
-                            Calculator
-                        </IonButton>
-                    </IonCardContent>
-
-                    <IonCardContent>
-                        <IonButton expand="block" routerLink='/Clickcounter'>
-                            <IonIcon icon={alarmOutline} slot="start" />
-                            Clickcounter
-                        </IonButton>
-                    </IonCardContent>
-                    
-
-                    
-                    <IonCardContent>
-                        <IonButton expand="block" routerLink='/todolist'>
-                            <IonIcon icon={clipboardOutline} slot="start" />
-                            todolist
-                        </IonButton>
-                    </IonCardContent>
-                    
-
-
-                </IonCard>
-
-            </IonContent>
-        </IonPage>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Home</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          
+          {/*Dynamic Search*/}
+          <IonSearchbar 
+            value={searchTerm} 
+            onIonInput={(e) => setSearchTerm(e.target.value ?? '')} 
+          />
+          
+          {cardData
+            .filter((card) => card.title.toLowerCase().includes(searchTerm.toLowerCase()))
+            .map((card, index) => (
+              <IonCard key={index} href={card.link}>
+                <IonCardHeader>
+                  <IonCardTitle>
+                    <IonGrid>
+                      <IonRow>
+                        <IonCol size="2">
+                          <IonIcon className="home-card-icon" icon={card.icon} color="primary" />
+                        </IonCol>
+                        <IonCol size="auto">
+                            <div className="home-card-title">{card.title}</div>
+                            {card.tags && Object.entries(card.tags).map(([key, icon], i) => (
+                              <IonIcon
+                                key={i}
+                                className="home-card-subicon"
+                                icon={icon}
+                                color="primary" // Set color as needed
+                              />
+                            ))}
+                          </IonCol>
+                      </IonRow>
+                    </IonGrid>
+                  </IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+          ))}
+        </IonContent>
+      </IonPage>
     );
 };
-
+  
 export default Home;
+  
